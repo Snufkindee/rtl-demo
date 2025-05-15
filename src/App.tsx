726dcbea9542f9f5ee96fcced46ccba2ffe63837
Snufkindee <kaminasri@gmail.com>
2025-05-15 16:04:41 +0300

    Add dropdown menu for demo purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import "./App.css";
@@ -21,20 +30,63 @@ function App() {
 
   return (
     <>
-      <header className="border-b border-border pl-4 py-2">
+      <header className="bg-accent-foreground border-b border-border pl-4 py-2">
         <div className="flex">
           <div className="w-full flex gap-4">
-            <h1 className="text-3xl font-bold">N & E</h1>
-            <p className="self-center text-sm">Quick RTL language demo</p>
+            <h1 className="text-3xl font-bold text-white">N & E</h1>
+            <p className="self-center text-sm text-primary">
+              {t("site_description")}
+            </p>
           </div>
 
           <div className="text-sm flex flex-row">
-            <Button variant="ghost" onClick={() => handleChangeLanguage("en")}>
-              EN
+            <div className="mr-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="default">{t("menu")}</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      Ninni
+                      <DropdownMenuShortcut>⌘N</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      Eino
+                      <DropdownMenuShortcut>⌘E</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      {t("share")}
+                      <DropdownMenuShortcut>⌘+S</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>{t("about")}</DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>GitHub</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <Button
+              variant="ghost"
+              onClick={() => handleChangeLanguage("en")}
+              className={`${
+                currentLanguage === "en" ? "text-primary" : "text-white"
+              }`}
+            >
+              {t("english")}
             </Button>
             <Separator orientation="vertical" className="w-1/2" />
-            <Button variant="ghost" onClick={() => handleChangeLanguage("fa")}>
-              FA
+            <Button
+              variant="ghost"
+              onClick={() => handleChangeLanguage("fa")}
+              className={`${
+                currentLanguage === "fa" ? "text-primary" : "text-white"
+              }`}
+            >
+              {t("farsi")}
             </Button>
           </div>
         </div>
@@ -55,8 +107,12 @@ function App() {
             <div className="flex flex-col justify-center gap-0.5">
               <h2 className="text-2xl font-bold">Ninni</h2>
               <p>
-                Ninni is a 2 year old cat that likes to eat everything
-                especially everything that is plastic.
+                Ninni is a 2 year old cat that likes to eat everything and
+                unfortunately everything that is made from plastic. <br />
+                She is a bit smaller than Eino but despite that much louder and
+                active. <br />
+                She is very vocal and social and let's you know if the food bowl
+                is empty.
               </p>
             </div>
           </Card>
@@ -66,8 +122,11 @@ function App() {
             <div className="flex flex-col justify-center gap-0.5">
               <h2 className="text-2xl font-bold">Eino</h2>
               <p>
-                Eino is a 5 year old cat that likes to fetch toys and chew on
-                some rope.
+                Eino is a 5 year old cat. Fetching toys and eating popcorn is
+                Eino's expertise.
+                <br />
+                He always watches and follows around Ninni to make sure that
+                she's fine whether Ninni likes it or not.
               </p>
             </div>
           </Card>
