0a6713509470e80d543977fc65e55971bf2afbd8
Snufkindee <kaminasri@gmail.com>
2025-05-16 12:25:52 +0300

    Final changes and updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useState } from "react";
+import i18n from "i18next";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import "./App.css";
 import eino from "./assets/Eino.jpg";
@@ -28,47 +20,23 @@ function App() {
     changeLanguage(newLanguage);
   };
 
+  useEffect(() => {
+    const dir = i18n.dir(i18n.language);
+    document.documentElement.dir = dir;
+  }, [i18n, i18n.language]);
+
   return (
     <>
-      <header className="bg-accent-foreground border-b border-border pl-4 py-2">
+      <header className="bg-accent-foreground border-b border-border ltr:pl-4 rtl:pr-4 py-2">
         <div className="flex">
           <div className="w-full flex gap-4">
             <h1 className="text-3xl font-bold text-white">N & E</h1>
-            <p className="self-center text-sm text-primary">
+            <p dir="ltr" className="self-center text-sm text-primary">
               {t("site_description")}
             </p>
           </div>
 
           <div className="text-sm flex flex-row">
-            <div className="mr-2">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="default">{t("menu")}</Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      Ninni
-                      <DropdownMenuShortcut>⌘N</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Eino
-                      <DropdownMenuShortcut>⌘E</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      {t("share")}
-                      <DropdownMenuShortcut>⌘+S</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>{t("about")}</DropdownMenuItem>
-                  </DropdownMenuGroup>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>GitHub</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
             <Button
               variant="ghost"
               onClick={() => handleChangeLanguage("en")}
@@ -96,38 +64,24 @@ function App() {
         <h1 className="self-center text-2xl font-bold mt-4 text-foreground">
           {t("title")}
         </h1>
-        <p className="self-center">
-          Ninni and Eino are Oriental shorhair cats that love to sleep and play
-          together
+        <p dir="ltr" className="self-center">
+          {t("description")}
         </p>
 
         <div className="flex flex-col gap-6">
           <Card className="flex flex-row justify-evenly">
             <img src={ninni} className="max-h-100 max-w-90 rounded-3xl" />
             <div className="flex flex-col justify-center gap-0.5">
-              <h2 className="text-2xl font-bold">Ninni</h2>
-              <p>
-                Ninni is a 2 year old cat that likes to eat everything and
-                unfortunately everything that is made from plastic. <br />
-                She is a bit smaller than Eino but despite that much louder and
-                active. <br />
-                She is very vocal and social and let's you know if the food bowl
-                is empty.
-              </p>
+              <h2 className="text-2xl font-bold">{t("ninni")}</h2>
+              <bdi className="w-3xl">{t("ninni_info")}</bdi>
             </div>
           </Card>
           <Card className="flex px-4 flex-row-reverse justify-evenly gap-4">
             <img src={eino} className="max-h-100 max-w-100 rounded-3xl" />
 
             <div className="flex flex-col justify-center gap-0.5">
-              <h2 className="text-2xl font-bold">Eino</h2>
-              <p>
-                Eino is a 5 year old cat. Fetching toys and eating popcorn is
-                Eino's expertise.
-                <br />
-                He always watches and follows around Ninni to make sure that
-                she's fine whether Ninni likes it or not.
-              </p>
+              <h2 className="text-2xl font-bold">{t("eino")}</h2>
+              <p className="w-3xl">{t("eino_info")}</p>
             </div>
           </Card>
         </div>
